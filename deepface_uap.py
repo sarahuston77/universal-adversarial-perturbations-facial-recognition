1150bbde0de1d9bc86dfe8985f12a92d68b54404
eshaready <munagala@umich.edu>
2026-04-06 00:36:45 -0400

    put code in - there's a bug right now we need to figure out to get everything working and compatible

--- deepface_uap.py
+++ deepface_uap.py
@@ -1,7 +1,13 @@
 import os
+from pathlib import Path
 import cv2 # btw to install this do pip install opencv-python
 import numpy as np
+from deepface import DeepFace 
+from deepface.models.demography import Gender 
 import tensorflow as tf
+from universal_pert import universal_perturbation
+
+print("Done importing...")
 
 # STEP ONE: LOAD DATA
 IMG_SIZE = 224 
@@ -35,4 +41,50 @@ def dataset_generator(folder):
         
         yield img, label
 
+def dataset_list(folder, limit=1000):
+    data = []
+    for i, (img, label) in enumerate(dataset_generator(folder)):
+        if i >= limit:
+            break
+        data.append((img[None, ...], label[None, ...]))
+    return data
+
 # STEPS TWO + THREE: GET GRADIENT + FEEDFORWARD FUNCTIONS
+# doing gender model at first bc its the simplest
+print("Loading gender model from deepface...")
+keras_model = Gender.load_model()
+keras_model.trainable = False
+
+# ok so UAP demo uses graphs
+print("Dealing with graph mode...")
+tf.compat.v1.disable_eager_execution()
+x = tf.compat.v1.placeholder(tf.float32, shape=(None, 224, 224, 3))
+y = tf.compat.v1.placeholder(tf.float32, shape=(None, 2))
+logits = keras_model(x)
+#! TODO: bug here^^^
+# "RuntimeError: Exception encountered when calling layer 'conv2d' (type Conv2D). resource: 
+# Attempting to capture an EagerTensor without building a function."
+loss = tf.keras.losses.categorical_crossentropy(y, logits)
+grad = tf.gradients(loss, x)[0]
+sess = tf.compat.v1.Session()
+print("Running session global variables intializer...")
+sess.run(tf.compat.v1.global_variables_initializer())
+
+# UAP wants the feedforward and gradient funcs now. so 
+def f(x_np):
+    return sess.run(logits, feed_dict={x: x_np})
+
+def grad_f(x_np, y_np):
+    return sess.run(grad, feed_dict={x: x_np, y: y_np})
+
+# STEP FOUR: actually run perturbation 
+print("Running UAP algorithm...")
+v = universal_perturbation(
+    dataset_list(dataset_generator(Path('data/crop_part1'))),
+    f,
+    grad_f,
+    delta=0.2,
+    max_iter_uni=10,
+    xi=10/255.0,
+    p=np.inf
+)
